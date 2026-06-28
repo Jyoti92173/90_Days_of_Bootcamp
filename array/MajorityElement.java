27f09d9f73d61fb764455238096141fb8a2e54c1
jyoti92173 <Jyoti53913@gmail.com>
2026-06-28 22:19:23 +0530

    Implement MajorityElement Using Boyer-Moore Voting Approach

--- array/MajorityElement.java
+++ array/MajorityElement.java
@@ -32,12 +32,25 @@ public class MajorityElement {
         }
         return res;
     }
+    // Boyer-Moore Voting Approach........
+    public int majorityElement3(int[] nums) {
+        int res = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                res = num;
+            }
+            count += (num == res) ? 1 : -1;
+        }
+        return res;
+    }
 
    public static void main(String[] args) {
         int [] nums = {5,5,1,1,1,5,5};
         MajorityElement m = new MajorityElement();
        System.out.println(m.majorityElements(nums));
        System.out.println(m.majorityElement2(nums));
+       System.out.println(m.majorityElement3(nums));
 
 
     }

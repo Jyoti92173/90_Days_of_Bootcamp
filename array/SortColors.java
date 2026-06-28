54931669743a69a0f3930ff48dd20564dcc58cf2
jyoti92173 <Jyoti53913@gmail.com>
2026-06-28 22:37:39 +0530

    Implement SortColors Using Brute-Force approach

---
+++ array/SortColors.java
@@ -0,0 +1,56 @@
+package array;
+
+import java.util.Arrays;
+
+public class SortColors {
+    // Brute-Force approach........
+    
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Count the occurrences of 0, 1, and 2
+        for (int num : nums) {
+            if (num == 0) {
+                count0++;
+            } else if (num == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        int index = 0;
+
+        // Fill all 0's
+        while (count0 > 0) {
+            nums[index] = 0;
+            index++;
+            count0--;
+        }
+
+        // Fill all 1's
+        while (count1 > 0) {
+            nums[index] = 1;
+            index++;
+            count1--;
+        }
+
+        // Fill all 2's
+        while (count2 > 0) {
+            nums[index] = 2;
+            index++;
+            count2--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums= {1,0,1,2};
+        SortColors sc = new SortColors();
+        sc.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}

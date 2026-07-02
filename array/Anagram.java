0fc90595cf181ba65391346ff7f76bb956db7989
jyoti92173 <Jyoti53913@gmail.com>
2026-07-02 17:36:55 +0530

    Implement Anagram Program Using Brute-Force and HashMap Approach

--- array/Anagram.java
+++ array/Anagram.java
@@ -1,5 +1,9 @@
 // Problem Statement : Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
 // An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+// Constraints:
+//
+//1 <= s.length, t.length <= 5 * 10^4
+//s and t consist of lowercase English letters.
 
 
 package array;

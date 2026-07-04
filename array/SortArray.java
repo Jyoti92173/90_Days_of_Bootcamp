e615a1560b69dd3310103ce9f4c05dd4e712b675
jyoti92173 <Jyoti53913@gmail.com>
2026-07-04 14:24:25 +0530

    Implement SortArray Program Using Brute-Force and Merge sort algorithm

--- array/SortArray.java
+++ array/SortArray.java
@@ -18,10 +18,65 @@ public class SortArray {
         return nums;
 
     }
+    // Two-Pointer Approach........
+    public static int[] sortArray2(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+    private static void mergeSort(int[] nums, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        // Sort left half
+        mergeSort(nums, left, mid);
+
+        // Sort right half
+        mergeSort(nums, mid + 1, right);
+
+        // Merge both halves
+        merge(nums, left, mid, right);
+    }
+    private static void merge(int[] nums, int left, int mid, int right) {
+
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        // Copy remaining elements from left half
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        // Copy remaining elements from right half
+        while (j <= right) {
+            temp[k++] = nums[j++];
+        }
+
+        // Copy back to original array
+        for (int p = 0; p < temp.length; p++) {
+            nums[left + p] = temp[p];
+        }
+    }
 
     public static void main(String[] args) {
         int[] nums = {10,9,1,1,1,2,3,1};
-
         System.out.println(Arrays.toString(sortArray(nums)));
+
+        System.out.println(Arrays.toString(sortArray2(nums)));
     }
 }

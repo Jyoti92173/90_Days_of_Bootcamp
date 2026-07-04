8f85067819a371e81a8b44c424bf9aca2f1d1d8a
jyoti92173 <Jyoti53913@gmail.com>
2026-07-04 14:26:52 +0530

    Implement SortArray Program Using Brute-Force and Merge sort algorithm

--- array/SortArray.java
+++ array/SortArray.java
@@ -1,3 +1,7 @@
+// Problem Statement: You are given an array of integers nums, sort the array in ascending order and return it.
+//
+//You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
 package array;
 
 import java.util.Arrays;

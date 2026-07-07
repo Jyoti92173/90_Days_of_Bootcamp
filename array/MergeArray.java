273a68ec0a8d46770898eeaa9e5ffed6564b5683
jyoti92173 <Jyoti53913@gmail.com>
2026-07-07 21:11:28 +0530

    Implement MergeArray Program Using Two-Pointer Approach

--- array/MergeArray.java
+++ array/MergeArray.java
@@ -10,6 +10,32 @@ public class MergeArray {
         }
         Arrays.sort(nums1);
     }
+    // Two-Pointer Approach.........
+    public static void merge1(int[] nums1, int m, int[] nums2, int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        // Copy remaining elements from nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
 
     public static void main(String[] args) {
         int[] nums1 = {10,20,20,40,0,0};
@@ -17,6 +43,7 @@ public class MergeArray {
         int[] nums2 = {1,2};
         int n = 2;
         merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
         System.out.println(Arrays.toString(nums1));
     }
 }

67c9bfd76b67b858e6f88c6c192106320800c81f
jyoti92173 <Jyoti53913@gmail.com>
2026-07-13 21:41:18 +0530

    Implement BinarySearch Program Using Brute-force with Linear Search Approach

--- array/BinarySearch.java
+++ array/BinarySearch.java
@@ -1,6 +1,6 @@
 //  Problem Statement : You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
 //  Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
-//  Your solution must run in O(logn) time.
+//  Your solution must run in O(logn)
 
 
 package array;

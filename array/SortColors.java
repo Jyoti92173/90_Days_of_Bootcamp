442d553ee709b823f153cf039c8f9605d65c31ad
jyoti92173 <Jyoti53913@gmail.com>
2026-07-14 20:27:27 +0530

    Implement SortColors Program Using Counting Sort Approach.

--- array/SortColors.java
+++ array/SortColors.java
@@ -1,3 +1,12 @@
+// Problem Statement : You are given an array nums consisting of n elements where each element is an integer representing a color:
+
+//0 represents red
+//1 represents whit
+//2 represents blue
+
+//Your task is to sort the array in-place such that elements of the same color are grouped together and arranged in the order: red (0), white (1), and then blue (2).
+//You must not use any built-in sorting functions to solve this problem.
+
 package array;
 
 import java.util.Arrays;

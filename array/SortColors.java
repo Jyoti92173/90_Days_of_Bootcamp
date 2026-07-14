6658bffd27ee8259815e24a0f569168b9d035434
jyoti92173 <Jyoti53913@gmail.com>
2026-07-14 20:59:30 +0530

    Implement SortColors Program Using Counting Sort Three Pointer and Three-Pointer - II Approach.

--- array/SortColors.java
+++ array/SortColors.java
@@ -94,6 +94,24 @@ public class SortColors {
         nums[i] = nums[j];
         nums[j] = temp;
     }
+
+    // Three Pointer Approach - II
+
+    public void sortColors4(int[] nums) {
+        int zero = 0, one = 0, two = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                nums[two++] = 2;
+                nums[one++] = 1;
+                nums[zero++] = 0;
+            } else if (nums[i] == 1) {
+                nums[two++] = 2;
+                nums[one++] = 1;
+            } else {
+                nums[two++] = 2;
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] nums= {1,0,1,2};
         SortColors sc = new SortColors();
@@ -103,6 +121,9 @@ public class SortColors {
         sc.sortColors2(nums);
         System.out.println(Arrays.toString(nums));
         sc.sortColors3(nums);
+        System.out.println(Arrays.toString(nums));
+
+        sc.sortColors4(nums);
         System.out.println(Arrays.toString(nums));
     }
 }

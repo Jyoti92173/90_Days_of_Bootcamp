e2c5b9f5ee444443ecb3feb62dd58b14f5e0435b
jyoti92173 <Jyoti53913@gmail.com>
2026-07-15 20:53:13 +0530

    Implement ProductOfArrayExceptItself Program using Brute-force and Prefix-Suffix Product Approach.

--- array/ProductOfArrayExceptItself.java
+++ array/ProductOfArrayExceptItself.java
@@ -1,3 +1,8 @@
+// Problem Statement : Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+// Each product is guaranteed to fit in a 32-bit integer.
+// Follow-up: Could you solve it inO(n)
+// O(n) time without using the division operation?
+
 package array;
 
 import java.util.Arrays;

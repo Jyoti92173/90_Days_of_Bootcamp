657095e6f0dab86cd75224591ea00a4a0329bec8
jyoti92173 <Jyoti53913@gmail.com>
2026-07-18 22:35:34 +0530

    Implement Sqrt Program Using Brute-Force Approach

---
+++ binarySearch/Sqrt.java
@@ -0,0 +1,19 @@
+package binarySearch;
+
+public class Sqrt {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+
+        int res = 1;
+        for (int i = 1; i <= x; i++) {
+            if ((long) i * i > x) {
+                return res;
+            }
+            res = i;
+        }
+
+        return res;f
+    }
+}

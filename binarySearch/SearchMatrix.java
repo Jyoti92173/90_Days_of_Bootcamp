f5260d9a45f7b8c2d26c55e76a955763014e5897
jyoti92173 <Jyoti53913@gmail.com>
2026-07-19 22:08:20 +0530

    Implement SearchMatrix Program Using Linear Search Approach.

--- binarySearch/SearchMatrix.java
+++ binarySearch/SearchMatrix.java
@@ -11,10 +11,22 @@ public class SearchMatrix {
         }
         return false;
     }
+
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                if (matrix[r][c] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         SearchMatrix searchMatrix = new SearchMatrix();
         int[][] matrix = {{1,2,4,8},{10,11,12,13},{14,20,30,40}};
         int target = 10;
         System.out.println(searchMatrix.searchMatrix(matrix, target));
+        System.out.println(searchMatrix.searchMatrix1(matrix, target));
     }
 }

4a522acc669f4fd4dc1a30298b7f6d55e5ec0043
jyoti92173 <Jyoti53913@gmail.com>
2026-07-22 11:47:06 +0530

    Implement PowerOfTwo Program Using Mathematical Approach.

---
+++ Math/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package Math;
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int x) {
+        if(x==0){
+            return false;
+        }
+        if(x==1){
+            return true;
+        }
+        if(x%2!=0){
+            return false;
+        }else{
+            return isPowerOfTwo(x/2);
+        }
+    }
+    public static void main(String[] args) {
+        int x = 16;
+        PowerOfTwo p = new PowerOfTwo();
+        System.out.println(p.isPowerOfTwo(x));
+    }
+}

4fd5a787cd88d9a07cdf2b98e7b0bab56cc9b6ff
jyoti92173 <Jyoti53913@gmail.com>
2026-07-22 11:57:17 +0530

    Implement PowerOfTwo Program Using Mathematical Approach.

--- Math/PowerOfTwo.java
+++ Math/PowerOfTwo.java
@@ -1,3 +1,6 @@
+// Problem Statement : Given an integer n, return true if it is a power of two. Otherwise, return false.
+//  An integer n is a power of two, if there exists an integer x such that n == 2x.
+
 package Math;
 
 public class PowerOfTwo {

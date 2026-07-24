6a1c884855f8ac00f5b77871708fbaf55825f2f1
jyoti92173 <Jyoti53913@gmail.com>
2026-07-24 21:46:00 +0530

    Implement PowerOfThree Program Using Recursion Approach.

--- Math/PowerOfThree.java
+++ Math/PowerOfThree.java
@@ -1,4 +1,21 @@
 package Math;
 
 public class PowerOfThree {
+    public boolean isPowerOfThree(int x) {
+        if (x == 1) {
+            return true;
+        }
+
+        if (x <= 0 || x % 3 != 0) {
+            return false;
+        }
+
+        return isPowerOfThree(x / 3);
+    }
+
+    public static void main(String[] args) {
+        int x = 81;
+        PowerOfThree p = new PowerOfThree();
+        System.out.println(p.isPowerOfThree(81));
+    }
 }

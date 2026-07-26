a6da653a22b33c212567e54f662c03961d0abbeb
jyoti92173 <Jyoti53913@gmail.com>
2026-07-26 18:11:46 +0530

    Implement FirstAndLastPosition Program Using Linear Search and Binary Search Approach.

--- array/FirstAndLastPosition.java
+++ array/FirstAndLastPosition.java
@@ -17,11 +17,51 @@ public class FirstAndLastPosition {
         return new int[]{first, last};
     }
 
+    public int[] searchRange2(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+
     public static void main(String[] args) {
     int[] nums = {5,7,7,8,8,10};
     int target = 8;
     FirstAndLastPosition obj = new FirstAndLastPosition();
         System.out.println(Arrays.toString(obj.searchRange(nums, target)));
+        System.out.println(Arrays.toString(obj.searchRange2(nums, target)));
 
     }
 

787cb9c9f5821b54536a006664ee6132e7bcee5b
jyoti92173 <Jyoti53913@gmail.com>
2026-07-28 13:25:54 +0530

    Implement ValidAnagram Program Using Brute Force - Sort and Compare Approach.

---
+++ string/ValidAnagram.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return  Arrays.equals(sArray, tArray);
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        ValidAnagram validAnagram = new ValidAnagram();
+        System.out.println(validAnagram.isAnagram(s,t));
+
+    }
+}

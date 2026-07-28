861fbd2b1c5727101cd2e179216225f5aa926394
jyoti92173 <Jyoti53913@gmail.com>
2026-07-28 15:24:55 +0530

    Implement LongestPalindrome Program using Brute Force Approach.

---
+++ string/LongestPalindrome.java
@@ -0,0 +1,35 @@
+package string;
+
+
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        int maxLength = 0;
+        String result = "";
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                String substring = s.substring(i, j+1);
+                if(isPalindrome(substring) && substring.length()>maxLength){
+                    result = substring;
+                    maxLength = substring.length();
+                }
+            }
+        }
+        return result;
+    }
+    private boolean isPalindrome(String s){
+        int left = 0;;
+        int right = s.length()-1;
+        while(left < right){
+            if(s.charAt(left++) != s.charAt(right--)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindrome lp = new LongestPalindrome();
+        System.out.println(lp.longestPalindrome("babad"));
+    }
+}

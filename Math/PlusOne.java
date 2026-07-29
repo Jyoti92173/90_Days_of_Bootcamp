7989df084a036709fb03885dafb0c8c9b2ffa0f4
jyoti92173 <Jyoti53913@gmail.com>
2026-07-29 21:47:19 +0530

    Implement PlusOne Program Using Brute Force Approach.

---
+++ Math/PlusOne.java
@@ -0,0 +1,37 @@
+package Math;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+
+
+        StringBuilder number = new StringBuilder();
+
+        for (int digit : digits) {
+            number.append(digit);
+        }
+
+
+        BigInteger num = new BigInteger(number.toString());
+
+
+        num = num.add(BigInteger.ONE);
+
+        String result = num.toString();
+
+        int[] answer = new int[result.length()];
+
+        for (int i = 0; i < result.length(); i++) {
+            answer[i] = result.charAt(i) - '0';
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        PlusOne p = new PlusOne();
+        System.out.println(Arrays.toString(p.plusOne(new int[]{1, 2, 3})));
+    }
+}

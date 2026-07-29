d5d87cdcf10c6c86d33300316fe528f33de17517
jyoti92173 <Jyoti53913@gmail.com>
2026-07-29 17:17:48 +0530

    Implement LongestPalindrome Program Using Brute Force Approach.

--- string/LongestPalindrome.java
+++ string/LongestPalindrome.java
@@ -1,6 +1,13 @@
+// Problem Statement : Given a string s, return the longest palindromic substring in s.
+//  Example 1:
+//  Input: s = "babad"
+//  Output: "bab"
+//  Explanation: "aba" is also a valid answer.
+//  Example 2:
+//  Input: s = "cbbd"
+//  Output: "bb"
+
 package string;
-
-
 
 public class LongestPalindrome {
     // Brute Force Approach..........

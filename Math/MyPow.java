92bd835c1758ba976d02f1f74c4ecc9a21923233
jyoti92173 <Jyoti53913@gmail.com>
2026-07-30 23:55:50 +0530

    Implement MyPow Program Using Brute Force and Binary Exponentiation Approach.

--- Math/MyPow.java
+++ Math/MyPow.java
@@ -39,7 +39,7 @@ public class MyPow {
         double x = 2.00000;
         int n = 5;
         MyPow mPow = new MyPow();
-        System.out.println("Binary Force :" + mPow.myPow(x,n));
+        System.out.println("Binary Force : " + mPow.myPow(x,n));
         int m = -3;
         System.out.println("Binary Exponentiation :" + mPow.myPow1(x,m));
     }

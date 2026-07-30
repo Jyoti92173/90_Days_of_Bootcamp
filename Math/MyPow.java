ee974040f6cebb8ae8a933c1bc9909620ee80413
jyoti92173 <Jyoti53913@gmail.com>
2026-07-30 15:57:27 +0530

    Implement MyPow Program Using Brute Force Approach.

---
+++ Math/MyPow.java
@@ -0,0 +1,23 @@
+package Math;
+
+public class MyPow {
+    public double myPow(double x, int n) {
+        if(x==0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        double result = 1;
+        for(int i =0; i<Math.abs(n);i++){
+            result *=x;
+        }
+        return n>=0 ? result:1 /result;
+    }
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = 5;
+        MyPow mPow = new MyPow();
+        System.out.println(mPow.myPow(x,n));
+    }
+}

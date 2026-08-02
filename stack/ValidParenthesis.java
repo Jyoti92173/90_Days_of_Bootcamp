bdb5ce64963a89f22fca7d5ad3cc14e20345e315
jyoti92173 <Jyoti53913@gmail.com>
2026-08-02 23:26:28 +0530

    Implement ValidParenthesis Program Using Brute Force Approach.

---
+++ stack/ValidParenthesis.java
@@ -0,0 +1,20 @@
+package stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+
+        return s.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "([{}])";
+        System.out.println(new ValidParenthesis().isValid(s));
+        
+
+    }
+}

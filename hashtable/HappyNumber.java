e4c7f31de09321af51cfa14556f61bb9ea1ab021
jyoti92173 <Jyoti53913@gmail.com>
2026-08-03 22:12:32 +0530

    Implement HappyNumber Program Using Brute Force Approach.

---
+++ hashtable/HappyNumber.java
@@ -0,0 +1,29 @@
+package hashtable;
+
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        int sum = n;
+        int x = n;
+        if(n == 1 || n == 7)
+            return true;
+        while(sum > 9) {
+            sum = 0;
+            while (x > 0) {
+                int d = x%10;
+                sum =sum+ d*d;
+                x=x/10;
+            }
+            if (sum == 1||sum == 7 )
+                return true;
+            x = sum;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        int n = 19;
+        HappyNumber h = new HappyNumber();
+        System.out.println(h.isHappy(n));
+
+    }
+}

a2bc00b36d47340b4d7238b98409a81b36fd456e
jyoti92173 <Jyoti53913@gmail.com>
2026-08-04 23:10:05 +0530

    Implement FindMissingElement Program Using Sorting Approach.

--- hashtable/FindMissingElement.java
+++ hashtable/FindMissingElement.java
@@ -1,6 +1,12 @@
-package hashtable;
+// You are given an integer array nums consisting of unique integers.
+//
+//Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.
+//
+//The smallest and largest integers of the original range are still present in nums.
+//Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
 
-import array.FirstAndLastPosition;
+
+package hashtable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,7 +54,7 @@ public class FindMissingElement {
 
         return res;
     }
-    
+
     public static void main(String[] args) {
         int[] nums= {1,4,2,5};
         int[] nums2 = {5,1};

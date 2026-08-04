1aee430798dcdab21c2b7f9aa3c86b73fc7a1ffb
jyoti92173 <Jyoti53913@gmail.com>
2026-08-04 23:07:18 +0530

    Implement FindMissingElement Program Using Sorting Approach.

--- hashtable/FindMissingElement.java
+++ hashtable/FindMissingElement.java
@@ -24,7 +24,7 @@ public class FindMissingElement {
 
         return res;
     }
-
+    // Boolean Array Approach.........
     public List<Integer> findMissingElements2(int[] nums) {
         List<Integer> res = new ArrayList<>();
         boolean[] contains = new boolean[101];

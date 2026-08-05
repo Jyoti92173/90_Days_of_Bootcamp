37444d4049caee5994de87ff472d59c64df7dbe4
jyoti92173 <Jyoti53913@gmail.com>
2026-08-05 22:46:26 +0530

    Implement ContainsDuplicate Program Using Brute Force Approach.

--- hashtable/ContainsDuplicate.java
+++ hashtable/ContainsDuplicate.java
@@ -13,6 +13,7 @@ public class ContainsDuplicate {
      }
     return false;
     }
+    
     public static void main(String[] args) {
         int[] nums = {1,2,3,1};
         ContainsDuplicate dc = new ContainsDuplicate();

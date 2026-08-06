462b2119070d021ac0a9e88bb3cfd1fb5017dd68
jyoti92173 <Jyoti53913@gmail.com>
2026-08-06 22:12:24 +0530

    Implement ReverseString Program Using Brute Force Approach.

---
+++ string/ReverseString.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    public void reverseString(char[] s){
+    int n =  s.length;
+    char[] temp = new char[n];
+    int j = 0;
+        // Copy in reverse order
+        for (int i = s.length - 1; i >= 0; i--) {
+            temp[j] = s[i];
+            j++;
+        }
+
+        // Copy back to original array
+        for (int i = 0; i < s.length; i++) {
+            s[i] = temp[i];
+        }
+
+    }
+    public static void main(String[] args) {
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        ReverseString r = new ReverseString();
+
+        r.reverseString(s); 
+
+        System.out.println(Arrays.toString(s));
+    }
+}

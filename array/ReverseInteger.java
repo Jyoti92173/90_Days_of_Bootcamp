285f0906b51aa74445962bb9ff46497559d7e71d
jyoti92173 <Jyoti53913@gmail.com>
2026-08-07 17:40:16 +0530

    Implement ReverseInteger Program using Mathematical Approach.

---
+++ array/ReverseInteger.java
@@ -0,0 +1,18 @@
+package array;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        long reverse = 0;
+        while (x != 0) {
+            int remainder =  x % 10;
+            reverse = reverse * 10 + remainder;
+            x /= 10;
+        }
+        return (int) reverse;
+    }
+    public static void main(String[] args) {
+        int x = 123;
+        ReverseInteger r = new ReverseInteger();
+        System.out.println(r.reverse(x));
+    }
+}

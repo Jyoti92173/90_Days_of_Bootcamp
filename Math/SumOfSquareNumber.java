cea9800cb933322c98905886d9517d72e6496b4f
jyoti92173 <Jyoti53913@gmail.com>
2026-08-07 21:58:21 +0530

    Implement SumOfSquareNumber Program Using Brute Force Approach.

---
+++ Math/SumOfSquareNumber.java
@@ -0,0 +1,21 @@
+package Math;
+
+public class SumOfSquareNumber {
+    public boolean judgeSquareSum(int c) {
+        for(long a = 0; a*a <= c; a++){
+            for(long b = 0; b*b <= c; b++){
+                if(a*a + b*b == c){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        int c = 5;
+        SumOfSquareNumber s = new SumOfSquareNumber();
+
+        System.out.println(s.judgeSquareSum(c));
+    }
+}

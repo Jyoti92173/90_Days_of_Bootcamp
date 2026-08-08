937964edc61ddda780864ce3ad076d7de16318b6
jyoti92173 <Jyoti53913@gmail.com>
2026-08-08 16:38:55 +0530

    Implement SumOfSquareNumber Program Using Brute Force and Two Pointer Approch.

--- Math/SumOfSquareNumber.java
+++ Math/SumOfSquareNumber.java
@@ -1,4 +1,4 @@
-// Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+// Problem Statement : Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
 
 package Math;
 

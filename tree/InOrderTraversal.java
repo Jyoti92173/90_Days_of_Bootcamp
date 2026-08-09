b3b6703c43cd349fb3644c0b0fcaebc938bd97d9
jyoti92173 <Jyoti53913@gmail.com>
2026-08-09 23:34:56 +0530

    Implement InOrderTraversal Program Using recursion Approach and DFS Approach

--- tree/InOrderTraversal.java
+++ tree/InOrderTraversal.java
@@ -24,8 +24,9 @@ public class InOrderTraversal {
         res.add(node.val);
         inorder(node.right);
     }
-    
+
     // Depth First Search Approach.......
+    
     public List<Integer> inorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();

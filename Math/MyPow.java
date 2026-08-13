f2428c35d68fee8449aaa1111926835516d77921
jyoti92173 <Jyoti53913@gmail.com>
2026-08-13 20:01:56 +0530

    Implement MyPow Program Using Brute Force and Binary Exponentiation Approach.

--- Math/MyPow.java
+++ Math/MyPow.java
@@ -17,7 +17,7 @@ public class MyPow {
         }
         return n>=0 ? result:1 /result;
     }
-// Binary Exponentiation Approach...............
+        // Binary Exponentiation Approach...............
 
     public double myPow1(double x, int n) {
         if (x == 0) return 0;

032ce28b6dae9e826a4cc29b5745ee0e36b6c159
jyoti92173 <Jyoti53913@gmail.com>
2026-08-17 22:23:33 +0530

    Implement LengthOfLastWord using Reverse Traversal Approach

--- string/LengthOfLastWord.java
+++ string/LengthOfLastWord.java
@@ -7,6 +7,7 @@ public class LengthOfLastWord {
 
     // Reverse Traversal Approach..........
     public int lengthOfLastWord(String s) {
+        
         int n = s.length();
         int count = 0;
         for (int i = n-1; i >= 0; i--) {
@@ -23,7 +24,7 @@ public class LengthOfLastWord {
     }
 
     public static void main(String[] args) {
-        
+
         String s = "Hello World";
         LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
         System.out.println(lengthOfLastWord.lengthOfLastWord(s));

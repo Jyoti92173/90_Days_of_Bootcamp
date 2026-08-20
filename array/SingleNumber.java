96e07082bfafe44f1a52ae67a70cb33638531251
jyoti92173 <Jyoti53913@gmail.com>
2026-08-20 23:23:35 +0530

    Implement SingleNumber Program Using Bit Manipulation Approach.

---
+++ array/SingleNumber.java
@@ -0,0 +1,18 @@
+package array;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+    int[] nums = {1,2,3,4,5};
+    SingleNumber singleNumber = new SingleNumber();
+    System.out.println(singleNumber.singleNumber(nums));
+    f
+    }
+}

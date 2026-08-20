12977d0caf026f01b7797a567491dbc0a25654a8
jyoti92173 <Jyoti53913@gmail.com>
2026-08-20 23:28:38 +0530

    Implement SingleNumber Program Using Brute Force and Bit Manipulation Approach.

--- array/SingleNumber.java
+++ array/SingleNumber.java
@@ -1,3 +1,19 @@
+// You are given a non-empty array of integers nums. Every integer appears twice except for one.
+//
+//Return the integer that appears only once.
+//
+//You must implement a solution with
+//O
+//(
+//n
+//)
+//O(n) runtime complexity and use only
+//O
+//(
+//1
+//)
+//O(1) extra space.
+
 package array;
 
 public class SingleNumber {

80c1b9b4c507bad97c88b49dca3bb859e7f40582
jyoti92173 <Jyoti53913@gmail.com>
2026-08-21 23:19:19 +0530

    Implement PostOrdereTraversal Program using depth first search approach

--- tree/PostOrderTraversal.java
+++ tree/PostOrderTraversal.java
@@ -1,5 +1,5 @@
 // You are given the root of a binary tree, return the postorder traversal of its nodes' values.
-//Input: root = [1,2,3,4,5,6,7]
+// Input: root = [1,2,3,4,5,6,7]
 
 // Output: [4,5,2,6,7,3,1]
 

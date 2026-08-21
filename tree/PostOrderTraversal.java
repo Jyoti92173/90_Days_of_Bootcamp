33eb295c9ece0d306d6be8b2066a1170197c5718
jyoti92173 <Jyoti53913@gmail.com>
2026-08-21 23:13:50 +0530

    Implement PostOrdereTraversal Program using depth first search approach

---
+++ tree/PostOrderTraversal.java
@@ -0,0 +1,37 @@
+package tree;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostOrderTraversal {
+    private List<Integer> res;
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        res = new ArrayList<>();
+        postorder(root);
+        return res;
+    }
+
+    private void postorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        res.add(node.val);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println(new PostOrderTraversal().postorderTraversal(root));
+
+
+    }
+}
+

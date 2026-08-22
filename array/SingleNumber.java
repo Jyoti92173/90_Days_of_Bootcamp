a8db2d229cc658fc833331f19f564c2cc211b4bb
jyoti92173 <Jyoti53913@gmail.com>
2026-08-22 23:40:58 +0530

    Implement SingleNumber Program using Brute Force and Bit Manipulation Approach

--- array/SingleNumber.java
+++ array/SingleNumber.java
@@ -21,8 +21,7 @@ public class SingleNumber {
         }
         return -1;
     }
-
-
+    
     // Bit Manipulation Approach................
     public int singleNumber1(int[] nums) {
         int result = 0;

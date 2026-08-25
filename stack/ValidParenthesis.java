c595bbd87d3486e94d29998677a0b3b6746146d8
jyoti92173 <Jyoti53913@gmail.com>
2026-08-25 22:58:21 +0530

    Implement ValidParenthesis Program using Brute Force Approach

--- stack/ValidParenthesis.java
+++ stack/ValidParenthesis.java
@@ -20,6 +20,7 @@ public class ValidParenthesis {
         }
         return s.isEmpty();
     }
+    
     public boolean isValid1(String s) {
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray())

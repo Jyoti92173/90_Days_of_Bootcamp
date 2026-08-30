b94b4d674b03404dd3a9b4dc2ae21ef8cb570df6
jyoti92173 <Jyoti53913@gmail.com>
2026-08-30 23:33:56 +0530

    add solution to merge two sorted linked lists

--- linkedList/MergeTwoList.java
+++ linkedList/MergeTwoList.java
@@ -1,4 +1,62 @@
 package linkedList;
 
 public class MergeTwoList {
+    public  ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+
+        if (list1 != null) {
+            node.next = list1;
+        } else {
+            node.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // List 1: 1 -> 2 -> 4
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // List 2: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        System.out.println("List 1:");
+        printList(list1);
+
+        System.out.println("List 2:");
+        printList(list2);
+
+        MergeTwoList obj = new MergeTwoList();
+
+        ListNode merged = obj.mergeTwoLists(list1, list2);
+
+        System.out.println("Merged List:");
+        printList(merged);
+
+    }
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+
+        System.out.println("null");
+    }
 }

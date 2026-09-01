8231e575ddaabcbb3a591ed735b2a2a9f815f405
jyoti92173 <Jyoti53913@gmail.com>
2026-09-01 22:48:05 +0530

    Implement BinarySearch Program Using Brute-Force Approach

--- binarySearch/BinarySearch.java
+++ binarySearch/BinarySearch.java
@@ -18,7 +18,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
-        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int[] nums ={1,2,3,4,5,6,7,8,9,10};
         int target = 9;
         System.out.println(bs.search(nums,target));
 

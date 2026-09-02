8893f5eedb296118b7018288d7b6dfc8c1d92947
jyoti92173 <Jyoti53913@gmail.com>
2026-09-02 23:29:36 +0530

    Implement SumOfSquareNumber Program Using Brute Force and Two Pointer Approach

--- Math/SumOfSquareNumber.java
+++ Math/SumOfSquareNumber.java
@@ -15,7 +15,7 @@ public class SumOfSquareNumber {
         return false;
     }
 
-    // Two Pointer Approach..
+    // Two Pointer Approach...
     public boolean judgeSquareSum1(int c) {
         long left = 0, right = (long) Math.sqrt(c);
         while (left <= right) {
